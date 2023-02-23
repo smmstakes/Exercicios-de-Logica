f24c76de8e809557fa96fff850d9d7045b624f47
smmstakes <matheusds014@gmail.com>
2023-02-23 15:28:13 -0300

    Realocação de arquivos

--- B2963.py
+++
@@ -1,8 +0,0 @@
-curupira = int(input()) * 300
-boitata = int(input()) * 1500
-boto = int(input()) * 600
-mapinguari = int(input()) * 1000
-iara = int(input()) * 150
-dona_chica = 225
-
-print(curupira + boitata + boto + mapinguari + iara + dona_chica)
